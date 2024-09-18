066258cf64770d57ea4774f4bdfeb83c35a0eab3
justniar <salsaniarno@gmail.com>
2024-09-18 11:07:47 +0700

    add decs

--- src/components/Program.tsx
+++ src/components/Program.tsx
@@ -27,15 +27,15 @@ const Program = () => {
     <div id="program" className="py-10 px-10 sm:px-10 relative">
       <div className="max-w-7xl mx-auto overflow-hidden" ref={scrollContainerRef}>
         <div className="inline-flex p-2 md:p-5 space-x-4 sm:space-x-20">
-          <ProgramCard src={math} judul="Prisma" deskripsi="Deskripsi singkat tentang program Prisma." />
-          <ProgramCard src={pracalis} judul="Abama" deskripsi="Deskripsi singkat tentang program Abama." />
-          <ProgramCard src={qiro} judul="Qiro'ati" deskripsi="Deskripsi singkat tentang program Qiro'ati." />
+          <ProgramCard src={math} judul="Prisma" deskripsi="Prisma Kalkulator Tangan adalah metode unik untuk menghitung dengan cepat menggunakan sepuluh jari tangan. Metode ini tidak memerlukan alat bantu hitung seperti kalkulator atau kertas, sehingga sangat praktis dan menyenangkan, terutama untuk anak-anak." />
+          <ProgramCard src={pracalis} judul="Abama" deskripsi="ABAMA adalah singkatan dari Anak Baca PRISMA. Ini adalah sebuah program pembelajaran yang dirancang khusus untuk membantu anak-anak usia Taman Kanak-Kanak (TK) dan Sekolah Dasar (SD) meningkatkan kemampuan membaca dan menulis mereka. Program ini merupakan bagian dari lembaga bimbingan belajar PRISMA yang juga terkenal dengan metode Prisma Kalkulator Tangan untuk belajar berhitung." />
+          <ProgramCard src={qiro} judul="Qiro'ati" deskripsi="Metode Qiroati adalah sebuah metode pembelajaran membaca Al-Qur'an yang dirancang untuk memudahkan seseorang, terutama anak-anak, dalam belajar membaca Al-Qur'an dengan benar dan lancar. Metode ini menekankan pada praktek langsung membaca Al-Qur'an tanpa perlu mengeja setiap hurufnya." />
           <ProgramCard src={math} judul="Sempoa" deskripsi="Deskripsi singkat tentang program Sempoa." />
           <ProgramCard src={math} judul="Cermat" deskripsi="Deskripsi singkat tentang program Cermat." />
           <ProgramCard src={math} judul="Tematik" deskripsi="Deskripsi singkat tentang program Tematik." />
           <ProgramCard src={pracalis} judul="Pracalis" deskripsi="Deskripsi singkat tentang program Pracalis." />
-          <ProgramCard src={english} judul="Bahasa Inggris" deskripsi="Deskripsi singkat tentang program Bahasa Inggris." />
-          <ProgramCard src={math} judul="Coding" deskripsi="Deskripsi singkat tentang program Coding." />
+          <ProgramCard src={english} judul="Bahasa Inggris" deskripsi="Brilliant English Course adalah salah satu lembaga kursus bahasa Inggris yang cukup terkenal, khususnya di kawasan Kampung Inggris, Pare, Kediri. Lembaga ini menawarkan program intensif belajar bahasa Inggris dengan metode yang dirancang untuk membantu peserta mencapai kemampuan berbahasa Inggris yang baik dalam waktu yang relatif singkat." />
+          <ProgramCard src={math} judul="Coding" deskripsi="Coding adalah proses menulis instruksi (kode) yang dapat dipahami oleh komputer untuk melakukan tugas tertentu. Ini melibatkan penggunaan bahasa pemrograman, yang merupakan seperangkat aturan dan sintaks yang digunakan untuk berkomunikasi dengan komputer." />
         </div>
       </div>
 
